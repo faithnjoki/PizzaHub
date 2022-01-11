c4ff5d1f5993f2a7ade4bd523b42219c42736269
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-01-11 18:04:38 +0300

    add images to static folder

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def  index():
+   return render_template ('index.html')
+
+@app.route('/about')
+def about():
+   return render_template ('about.html')
+
+@app.route('/contact')
+def contact():
+   return render_template ('contact.html')
+
+@app.route('/menu')
+def menu():
+   return render_template ('menu.html')
