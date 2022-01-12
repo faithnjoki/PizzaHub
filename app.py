94bd9375ba5df792483932e314f53b7715c44e8f
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-01-12 12:29:47 +0300

    edit index file

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask
 from flask import render_template
+from flask.wrappers import Request
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
 
 app = Flask(__name__)
+app.config['SQLA_DATABASE_URI'] = 'sqlite:////tmp/pizza.db'
+
+# This avoids the notifications on terminal
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# initialize the db
+db = SQLAlchemy(app)
+
+# create a model
+class Pizza(db.Model):
+      id = db.Column(db.Integer, primary_key=True)
+      name = db.Column(db.String, nullable = False)
+
+
+      def __repr__(self):
+        return '<Pizza %r>' % self.name
+
+
+
 
 @app.route('/')
 def  index():
@@ -15,8 +37,11 @@ def about():
 def contact():
    return render_template ('contact.html')
 
-@app.route('/menu')
+@app.route('/menu', methods=['POST','GET'])
 def menu():
+   if request.method == 'POST':
+      pizzatype = request.form[' ']
+
    return render_template ('menu.html')
 
 @app.route('/cart')
